5906a8f6da8320da1b6916258fc0532c13df8fe4
momoshenchi <982060958@qq.com>
2024-11-27 15:45:13 +0800

    使用qwen2.5重新评测

--- race_tourism/eval.py
+++ race_tourism/eval.py
@@ -4,8 +4,10 @@ import traceback
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = "Qwen/Qwen2-7B-Instruct"
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+model_name = "Qwen/Qwen2.5-7B-Instruct"
+log_file = "run_log_qwen25.jsonl"  # 日志
+ans_file = "model_logits_qwen25.jsonl"  # 推理结果
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 tokenizer = AutoTokenizer.from_pretrained(model_name,padding_side="left")
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map=device, torch_dtype="auto")
 
@@ -14,7 +16,7 @@ def generate_response(inp_list, unique_id_list, max_new_tokens=2048):
         input_ids = []
         for inp in inp_list:
             messages = [
-            {"role": "system", "content": "你是一位专业且有帮助的AI助手，专门回答与旅游相关的各种问题，包括选择题和开放式问题。对于选择题，请先直接给出正确答案，然后详细说明理由，并逐一分析其他选项的优缺点。你的回答应基于最新、最准确的旅游信息，确保内容严谨、准确。回答时，语言应与问题的主要语言保持一致。"},
+            {"role": "system", "content": "你是一位专业且有帮助的AI助手，专门回答与旅游、地理、交通相关的各种问题，包括选择题和开放式问题。对于选择题，请先直接给出正确答案，然后详细说明理由，并逐一分析其他选项的优缺点。你的回答应确保内容严谨、准确，语言应与问题中的主要语言保持一致。"},
             {"role": "user", "content": inp}
                 ]
             input_id = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
@@ -58,9 +60,6 @@ def load_test_data(test_file):
         for line in f:
             test_data.append(json.loads(line.strip()))
     return test_data
-
-log_file = "run_log.jsonl"  # 日志
-ans_file = "model_logits.jsonl"  # 推理结果
 
 # 加载测试数据
 test_data = load_test_data('eval_only_query.jsonl')
